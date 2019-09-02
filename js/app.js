efd125464f73e8d85302a6a59139540654a9daac
Carmela <carme.rl@hotmail.com>
2019-09-02 12:14:33 -0300

    Se agrega botón y función BORRAR a la lista de tareas

--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
 
+
+// EVENT LISTENERS
+
 eventListeners();
 
 function eventListeners(){
 
     document.querySelector('#btnAddTask').addEventListener('click', fAddTaskList);
     
+    taskList.addEventListener('click', fDeleteTask);
 }
 
 
-function fAddTaskList(){
+
+
+// FUNCIONES
+function fAddTaskList(e){
 
     const tarea = document.getElementById('txtTask').value;
 
-    console.log(tarea);
+    const btnBorrar = document.createElement('a');
+    btnBorrar.classList = "borrar-tarea";
+    btnBorrar.innerText = "X";
+    
+    const li = document.createElement("li");
+    li.innerText = tarea;
+    li.appendChild(btnBorrar);
+    taskList.appendChild(li);
+}
+
+
+function fDeleteTask(e){
+    
+
+    if(e.target.className === "borrar-tarea"){
+        e.target.parentElement.remove();
+        
+    }
+
+
 
 }
